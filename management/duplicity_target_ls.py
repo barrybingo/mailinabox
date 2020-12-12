84c752f1ffd531bdfc5bbedecb971b7025eeeb21
barrybingo <millions.of.stones@gmail.com>
2020-12-12 11:54:44 +0000

    Duplicity updated to python 3 so update script

--- management/duplicity_target_ls.py
+++ management/duplicity_target_ls.py
@@ -1,10 +1,12 @@
-#! /usr/bin/python
+#!/usr/bin/python3
 #
 # Will print a list of file names in a duplicity target_url
-# duplicity is python 2.7 and this script uses it's modules
+# duplicity is python3 (on ubuntu 18.04.1) and this script uses it's modules
 # Note that backends make much use of environmental variables
 #
+#
 # example usage:
+#    pip3 install PyDrive
 #    GOOGLE_DRIVE_SETTINGS=pydrive-settings.yaml ./duplicity_target_ls.py \
 #                           "gdocs://example@gmail.com/duplicity"
 #
@@ -13,6 +15,11 @@
 # Depending on backend, target_url may be created if not already existing
 # Also, gank output if a folder exists in target_url as folder has no size attibute
 #
+# Notes:
+# GDrive backup failing with
+#   Attempt 1 failed. RedirectMissingLocation: Redirected but the response is missing a Location: header
+#   see:   https://github.com/googleapis/google-api-python-client/issues/803
+#   fix:   pip3 install -U httplib2==0.15.0
 
 import sys
 from duplicity import log
@@ -30,21 +37,21 @@ def duplicity_ls(target_url):
 
         for name,info in sorted(infos.items(), key=lambda x: x[0]):
             size = info['size']
-            print "%s\t%d" % (name, size)
+            print("%s\t%d" % (name, size))
 
         return 0
 
-    except Exception, e:
-        print str(e)
+    except Exception as e:
+        print(str(e))
         return 1
 
 if __name__== "__main__":
     try:
         log.setup()
         log.setverbosity(log.WARNING)
-    except Exception, e:
+    except Exception as e:
         print("ERROR: Unable to setup duplicity log")
-        print str(e)
+        print(str(e))
         sys.exit(1)
 
     if len(sys.argv) != 2:
